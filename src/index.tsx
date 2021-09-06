8144e71949276342110962bc44fa8c0d0d5636a7
miki <mikiymk001@gmail.com>
2021-09-06 17:56:26 +0900

    delete no react code

--- src/index.tsx
+++ src/index.tsx
@@ -12,104 +12,6 @@ const SCOPES = [
     'https://www.googleapis.com/auth/drive.metadata.readonly',
 ].join(' ');
 
-const authorizeButton = document.getElementById('authorize_button')!;
-const signoutButton = document.getElementById('signout_button')!;
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-async function initClient() {
-    try {
-        await gapi.client.init({
-            apiKey: API_KEY,
-            clientId: CLIENT_ID,
-            discoveryDocs: DISCOVERY_DOCS,
-            scope: SCOPES
-        });
-
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
-    } catch (error) {
-        appendPre(JSON.stringify(error, null, 2));
-    }
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn: boolean) {
-    if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-        listFiles();
-    } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
-    }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event: MouseEvent) {
-    gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event: MouseEvent) {
-    gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message: string) {
-    const pre = document.getElementById('content')!;
-    const textContent = document.createTextNode(message + '\n');
-    pre.appendChild(textContent);
-}
-
-function appendMusic(name: string, id: string) {
-    const ul = document.getElementById('musics')!;
-    const li = document.createElement('li');
-    const button = document.createElement('button');
-    button.appendChild(document.createTextNode('play'));
-    button.addEventListener('click', async event => {
-        console.log(`playing ${name}`);
-        const responce = await gapi.client.drive.files.get({
-            'fileId': id,
-            'fields': '*',
-        });
-        console.log(responce.result.webContentLink);
-        let audio = new Audio(responce.result.webContentLink);
-        audio.play();
-    });
-
-    li.appendChild(document.createTextNode(`${name} (${id})`));
-    li.appendChild(button);
-
-    ul.appendChild(li);
-}
-
 async function getFiles() {
     const request: {
         fields: string,
@@ -138,24 +40,6 @@ async function getFiles() {
         }
     }
     return files;
-}
-
-/**
- * Print files.
- */
-async function listFiles() {
-    appendPre('Files:');
-
-    const files = await getFiles();
-
-    if (files && files.length > 0) {
-        for (var i = 0; i < files.length; i++) {
-            const file = files[i];
-            appendMusic(file.name ?? '', file.id ?? '');
-        }
-    } else {
-        appendPre('No files found.');
-    }
 }
 
 class MusicPlayer extends React.Component<{}, { isSignedIn: boolean, files: { name: string, id: string, link: string }[], preText: string }> {
@@ -246,7 +130,5 @@ const MusicListItem: React.FunctionComponent<{ name: string, id: string, link: s
         <button onClick={playing}>play</button>
     </li>;
 }
-
-handleClientLoad();
 
 ReactDOM.render(<MusicPlayer />, document.getElementById('root'));
