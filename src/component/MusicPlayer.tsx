99c4d4c815fc59416ee16ac8af32393395494013
miki <mikiymk001@gmail.com>
2021-09-28 23:33:09 +0900

    change authorize component use state and effect in function component

--- src/component/MusicPlayer.tsx
+++ src/component/MusicPlayer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { loadAndInit, getFiles, signOut, signIn } from "../api";
+import { getFiles } from "../api";
 import { File } from "../type";
 import { PlayingInfo } from "./PlayManager";
 import { MusicList } from "./MusicList";
 import { NowPlayingList } from "./PlayingList";
 import { AudioPlayer } from "../player";
 import { AudioList } from "../list";
+import { Authorize } from "./Authorize";
 
 /**
  * react component root.
@@ -15,7 +16,6 @@ export class MusicPlayer extends React.Component<
   {
     isSignedIn: boolean;
     files: File[];
-    preText: string;
     context: AudioContext;
     player: AudioPlayer;
     list: AudioList;
@@ -31,7 +31,6 @@ export class MusicPlayer extends React.Component<
     this.state = {
       isSignedIn: false,
       files: file,
-      preText: "",
       context: ctx,
       player: new AudioPlayer(ctx),
       list: new AudioList(ctx, file, 0),
@@ -42,11 +41,6 @@ export class MusicPlayer extends React.Component<
   }
 
   componentDidMount() {
-    loadAndInit(
-      isSignedIn => this.updateSigninStatus(isSignedIn),
-      error => this.appendPre(JSON.stringify(error, null, 2))
-    );
-
     this.state.player.onSetCurrentTime = currentTime =>
       this.setState(state => {
         const newState = {
@@ -78,28 +72,15 @@ export class MusicPlayer extends React.Component<
   }
 
   /**
-   * of sign in status,
-   * @param isSignedIn if google signed in
+   * called when sign in at google
    */
-  updateSigninStatus(isSignedIn: boolean) {
-    this.setState({ isSignedIn });
-    if (isSignedIn) {
-      getFiles(files =>
-        this.setState(state => {
-          state.files.push(...files);
-          return state;
-        })
-      );
-    }
-  }
-
-  /**
-   * message append to pre element
-   * @param message append message
-   */
-  appendPre(message: string) {
-    const preText = this.state.preText + message + "\n";
-    this.setState({ preText });
+  onSignIn() {
+    getFiles(files =>
+      this.setState(state => {
+        state.files.push(...files);
+        return state;
+      })
+    );
   }
 
   playWithIndex(index: number) {
@@ -107,18 +88,6 @@ export class MusicPlayer extends React.Component<
       state.list.setIndex(index);
       return state;
     });
-  }
-
-  setPlaying(isPlay: boolean) {
-    if (isPlay) {
-      this.state.player.play();
-    } else {
-      this.state.player.pause();
-    }
-  }
-
-  seek(time: number) {
-    this.state.player.seek(time);
   }
 
   render() {
@@ -129,11 +98,11 @@ export class MusicPlayer extends React.Component<
           duration={this.state.duration}
           currentTime={this.state.currentTime}
           paused={this.state.paused}
-          seek={time => this.seek(time)}
-          play={() => this.setPlaying(true)}
-          pause={() => this.setPlaying(false)}
+          seek={time => this.state.player.seek(time)}
+          play={() => this.state.player.play()}
+          pause={() => this.state.player.pause()}
         />
-        <AuthButton isSignedIn={this.state.isSignedIn} />
+        <Authorize onSignIn={() => this.onSignIn()} />
         <MusicList
           files={this.state.files}
           play={index => this.playWithIndex(index)}
@@ -142,21 +111,7 @@ export class MusicPlayer extends React.Component<
           list={this.state.list.list}
           playingIndex={this.state.list.index}
         />
-        <pre>{this.state.preText}</pre>
       </div>
     );
   }
 }
-
-/**
- * authorize or sign out button
- * @param props compontnt props
- * @returns react render
- */
-const AuthButton: React.FC<{ isSignedIn: boolean }> = ({ isSignedIn }) => {
-  if (isSignedIn) {
-    return <button onClick={signOut}>Sign Out</button>;
-  } else {
-    return <button onClick={signIn}>Authorize</button>;
-  }
-};
