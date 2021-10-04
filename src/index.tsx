6aad62d096f3a533acd06520be5b00d5d5b6962c
miki <mikiymk001@gmail.com>
2021-10-04 17:49:36 +0900

    add scss style
    use style with scss compile to css test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { MusicPlayer } from "./component/MusicPlayer";
 import React from "react";
 import ReactDOM from "react-dom";
+import "./scss/style.scss";
 
 ReactDOM.render(<MusicPlayer />, document.getElementById("root"));
--- webpack.common.js
+++ webpack.common.js
@@ -2,17 +2,25 @@
 
 const path = require("path");
 
+//MiniCssExtractPlugin の読み込み
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.tsx",
-  devtool: 'source-map',
+  devtool: "source-map",
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: path.resolve(__dirname, "javascript"),
   },
   plugins: [
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+
+    new MiniCssExtractPlugin({
+      // 抽出する CSS のファイル名
+      filename: "style.css",
+    }),
   ],
   module: {
     rules: [
@@ -29,10 +37,45 @@ module.exports = {
         enforce: "pre",
         use: ["source-map-loader"],
       },
+      {
+        // 対象となるファイルの拡張子(scss)
+        test: /\.scss$/,
+        // Sassファイルの読み込みとコンパイル
+        use: [
+          // CSSファイルを抽出するように MiniCssExtractPlugin のローダーを指定
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          // CSSをバンドルするためのローダー
+          {
+            loader: "css-loader",
+            options: {
+              //URL の解決を無効に
+              url: false,
+              // ソースマップを有効に
+              sourceMap: true,
+            },
+          },
+          // Sass を CSS へ変換するローダー
+          {
+            loader: "sass-loader",
+            options: {
+              // dart-sass を優先
+              implementation: require("sass"),
+              sassOptions: {
+                // fibers を使わない場合は以下で false を指定
+                fiber: false, // require('fibers'),
+              },
+              // ソースマップを有効に
+              sourceMap: true,
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
-    modules: ['node_modules'],
+    modules: ["node_modules"],
   },
 };
