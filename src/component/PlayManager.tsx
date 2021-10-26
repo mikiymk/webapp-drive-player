dddeb10e0856663d7e5656accdbe1c382d0cbaef
miki <mikiymk001@gmail.com>
2021-10-26 17:30:45 +0900

    change seekbar
    if plays, move to seek to click up to seek time

--- src/component/PlayManager.tsx
+++ src/component/PlayManager.tsx
@@ -1,5 +1,5 @@
 import { formatTime } from "../format";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 /**
  * now playing audio info view
@@ -45,15 +45,44 @@ const SeekBar: React.FC<{
   duration: number;
   time: number;
   seek: (time: number) => void;
-}> = ({ duration, time, seek }) => (
-  <input
-    type="range"
-    min="0"
-    max={duration * 1000}
-    value={time * 1000}
-    onChange={event => seek(parseInt(event.target.value, 10) / 1000)}
-  />
-);
+}> = ({ duration, time, seek }) => {
+  const [seekTime, setSeekTime] = useState(0);
+  const [click, setClick] = useState(false);
+
+  useEffect(() => {
+    if (!click) {
+      setSeekTime(Math.round(time * 1000));
+    }
+  }, [time]);
+
+  const onClickDown = () => {
+    setClick(true);
+  };
+
+  const onClickUp = () => {
+    setClick(false);
+    seek(seekTime / 1000);
+  };
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+    setSeekTime(parseInt(event.target.value, 10));
+  };
+
+  return (
+    <input
+      type="range"
+      min="0"
+      max={duration * 1000}
+      value={seekTime}
+      onChange={onChange}
+      onInput={onChange}
+      onMouseDown={onClickDown}
+      onMouseUp={onClickUp}
+      onTouchStart={onClickDown}
+      onTouchEnd={onClickUp}
+    />
+  );
+};
 
 const useToggle = <T,>(changes: T[]): [T, () => void] => {
   const [index, setIndex] = useState(0);
