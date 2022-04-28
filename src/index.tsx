20119813252fa94366ac56c4f772d2df1e4a0dbe
mikiymk <mikiymk001@gmail.com>
2022-04-28 12:14:14 +0900

    #49 wasmを追加する

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import "~/css/style.css";
 
 import MusicPlayer from "~/components/MusicPlayer";
 
+import init, { greet } from "../wasm-sql/pkg";
+
+init().then(() => greet());
+
 const root = document.getElementById("root");
 
 if (root !== null) {
