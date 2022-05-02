e96de59b280d89e1b4b70d6a35a2f8ffa3d4a54c
mikiymk <mikiymk001@gmail.com>
2022-05-02 20:31:40 +0900

    #49 music-metadataのpolyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import path from "path";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import solidPlugin from "vite-plugin-solid";
 import eslintPlugin from "vite-plugin-eslint";
 
 const config = defineConfig(({ command }) => {
+  const config: UserConfig = {
+    plugins: [vanillaExtractPlugin(), solidPlugin()],
+    resolve: {
+      alias: { "~/": path.join(__dirname, "src/") },
+    },
+  };
+
   if (command === "serve") {
-    return {
-      build: {
-        sourcemap: true,
-      },
-      plugins: [vanillaExtractPlugin(), solidPlugin(), eslintPlugin()],
-      resolve: {
-        alias: {
-          "~/": path.join(__dirname, "src/"),
-        },
-      },
-    };
-  } else {
-    return {
-      plugins: [vanillaExtractPlugin(), solidPlugin()],
-      resolve: {
-        alias: {
-          "~/": path.join(__dirname, "src/"),
-        },
-      },
-    };
+    config.build = { sourcemap: true };
+    config.plugins.push(eslintPlugin());
   }
+  return config;
 });
 
 export default config;
