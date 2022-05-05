f90cf1dad309e4076bc0d20b8a1de3688fc93b12
mikiymk <mikiymk001@gmail.com>
2022-05-05 17:53:16 +0900

    #54 テストを書く

---
+++ src/audio/__tests__/AudioInfo.test.ts
@@ -0,0 +1,51 @@
+import { test, expect, vi } from "vitest";
+import AudioInfo from "../AudioInfo";
+
+test("AudioInfo empty", () => {
+  const emptyInfo = AudioInfo.getEmptyInfo();
+
+  expect(emptyInfo).toHaveProperty("title");
+  expect(emptyInfo).toHaveProperty("artists");
+  expect(emptyInfo).toHaveProperty("album");
+  expect(emptyInfo).toHaveProperty("albumartist");
+  expect(emptyInfo).toHaveProperty("track");
+  expect(emptyInfo).toHaveProperty("disk");
+  expect(emptyInfo).toHaveProperty("date");
+  expect(emptyInfo).toHaveProperty("genre");
+  expect(emptyInfo).toHaveProperty("picture");
+  expect(emptyInfo).toHaveProperty("sort");
+});
+
+test("AudioInfo blob", async () => {
+  const mock = vi.spyOn(console, "log").mockImplementation(() => void 0);
+  const blobInfo = await AudioInfo.getInfo(new Blob());
+  expect(mock).toHaveBeenCalledTimes(2);
+
+  expect(blobInfo).toHaveProperty("title");
+  expect(blobInfo).toHaveProperty("artists");
+  expect(blobInfo).toHaveProperty("album");
+  expect(blobInfo).toHaveProperty("albumartist");
+  expect(blobInfo).toHaveProperty("track");
+  expect(blobInfo).toHaveProperty("disk");
+  expect(blobInfo).toHaveProperty("date");
+  expect(blobInfo).toHaveProperty("genre");
+  expect(blobInfo).toHaveProperty("picture");
+  expect(blobInfo).toHaveProperty("sort");
+  expect(blobInfo).toEqual(AudioInfo.getEmptyInfo());
+});
+
+test("AudioInfo select", () => {
+  const selectInfo = AudioInfo.selectInfo({});
+
+  expect(selectInfo).toHaveProperty("title");
+  expect(selectInfo).toHaveProperty("artists");
+  expect(selectInfo).toHaveProperty("album");
+  expect(selectInfo).toHaveProperty("albumartist");
+  expect(selectInfo).toHaveProperty("track");
+  expect(selectInfo).toHaveProperty("disk");
+  expect(selectInfo).toHaveProperty("date");
+  expect(selectInfo).toHaveProperty("genre");
+  expect(selectInfo).toHaveProperty("picture");
+  expect(selectInfo).toHaveProperty("sort");
+  expect(selectInfo).toEqual(AudioInfo.getEmptyInfo());
+});
