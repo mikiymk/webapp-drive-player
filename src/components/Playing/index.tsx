ed5a6ead9046247bb2abcce43fa4d650a4353a8d
mikiymk <mikiymk001@gmail.com>
2022-05-11 15:58:08 +0900

    #66 曲の情報を表示するようにした

--- src/components/Playing/index.tsx
+++ src/components/Playing/index.tsx
@@ -1,14 +1,12 @@
-import { For, Show } from "solid-js";
+import { Show } from "solid-js";
 
 import type AudioInfo from "~/audio/AudioInfo";
-import { useAudios } from "~/hooks/createFiles";
 import useJacket from "~/hooks/useJacket";
 import NoImage from "./NoImage";
 import { styleImage, stylePlaying } from "./style.css";
 
 type Props = {
   info: AudioInfo;
-  playingList: Iterable<string>;
 };
 
 /**
@@ -16,22 +14,30 @@ type Props = {
  */
 const PlayingInfo = (props: Props) => {
   const jacket = useJacket(() => props.info.picture);
-  const audios = useAudios();
-  const audioTitle = (id: string) => {
-    return audios.audios[id]?.title ?? "";
-  };
 
   return (
     <div class={stylePlaying}>
-      <span>{props.info.album}</span>
       <Show when={jacket()} fallback={<NoImage />}>
         {jacket => <img src={jacket} alt="album jacket" class={styleImage} />}
       </Show>
-      <ol>
-        <For each={Array.from(props.playingList)}>
-          {id => <li>{audioTitle(id)}</li>}
-        </For>
-      </ol>
+      <dl>
+        <dt>Album</dt>
+        <dd>{props.info.album}</dd>
+        <dt>Album Artist</dt>
+        <dd>{props.info.albumartist}</dd>
+        <dt>Title</dt>
+        <dd>{props.info.title}</dd>
+        <dt>Artist</dt>
+        <dd>{props.info.artists}</dd>
+        <dt>Disk</dt>
+        <dd>
+          {props.info.disk.no}/{props.info.disk.of}
+        </dd>
+        <dt>Track</dt>
+        <dd>
+          {props.info.track.no}/{props.info.track.of}
+        </dd>
+      </dl>
     </div>
   );
 };
--- src/components/Playing/style.css.ts
+++ src/components/Playing/style.css.ts
@@ -1,6 +1,7 @@
 import { style } from "@vanilla-extract/css";
 
 export const stylePlaying = style({
+  display: "flex",
   height: "100%",
 });
 
