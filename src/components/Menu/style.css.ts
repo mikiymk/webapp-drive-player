0f1d34e9f1e1cb09333985f471b7113ca2729011
mikiymk <mikiymk001@gmail.com>
2022-05-13 21:46:05 +0900

    #64 メニューを上部に移動する

--- src/components/Menu/style.css.ts
+++ src/components/Menu/style.css.ts
@@ -2,20 +2,22 @@ import { style } from "@vanilla-extract/css";
 
 export const styleMenu = style({
   display: "flex",
+  flexDirection: "column",
   flex: "0 1 100vh",
   overflow: "hidden",
 });
 
 export const styleNav = style({
   flex: "0 0 max-content",
-  paddingTop: "4rem",
   backgroundColor: "rgb(165, 165, 165)",
 });
 
 export const styleNavItem = style({
+  display: "inline-block",
   cursor: "pointer",
   margin: "0.3rem 0rem",
   padding: "0.2rem 0.5rem",
+  whiteSpace: "nowrap",
 
   selectors: {
     "&:hover": {
