3d4a01a0f2400441a845fb085ea0b2a3c632b39d
ymk <65121726+mikiymk@users.noreply.github.com>
2022-05-29 23:30:05 +0900

    #86 テストを通るようにする

--- src/__tests__/format.test.ts
+++ src/__tests__/format.test.ts
@@ -3,29 +3,29 @@ import { test, expect } from "vitest";
 import { formatTime } from "../format";
 
 test("1 second", () => {
-  expect(formatTime(1.0)).toBe("00:01.000");
+  expect(formatTime(1.0)).toBe("00:01");
 });
 
 test("59 seconds", () => {
-  expect(formatTime(59.0)).toBe("00:59.000");
+  expect(formatTime(59.0)).toBe("00:59");
 });
 
 test("1 minute 1 second", () => {
-  expect(formatTime(61.0)).toBe("01:01.000");
+  expect(formatTime(61.0)).toBe("01:01");
 });
 
 test("100 minutes 1 second", () => {
-  expect(formatTime(6001.0)).toBe("100:01.000");
+  expect(formatTime(6001.0)).toBe("100:01");
 });
 
 test("1 second 001", () => {
-  expect(formatTime(1.001)).toBe("00:01.001");
+  expect(formatTime(1.001)).toBe("00:01");
 });
 
 test("1 second 999", () => {
-  expect(formatTime(1.999)).toBe("00:01.999");
+  expect(formatTime(1.999)).toBe("00:01");
 });
 
 test("round 1 second 001", () => {
-  expect(formatTime(1.0014)).toBe("00:01.001");
+  expect(formatTime(1.0014)).toBe("00:01");
 });
