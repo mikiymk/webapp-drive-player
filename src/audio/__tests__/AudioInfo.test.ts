d70aef228584c992fce90e11725b41d952232989
mikiymk <mikiymk001@gmail.com>
2022-11-01 21:20:29 +0900

    #100 情報オブジェクトに曲の長さを含める

--- src/audio/__tests__/AudioInfo.test.ts
+++ src/audio/__tests__/AudioInfo.test.ts
@@ -11,26 +11,7 @@ test("AudioInfo empty", () => {
   expect(emptyInfo).toHaveProperty("albumartist");
   expect(emptyInfo).toHaveProperty("track");
   expect(emptyInfo).toHaveProperty("disk");
-  expect(emptyInfo).toHaveProperty("date");
-  expect(emptyInfo).toHaveProperty("genre");
+  expect(emptyInfo).toHaveProperty("duration");
   expect(emptyInfo).toHaveProperty("picture");
   expect(emptyInfo).toHaveProperty("sort");
 });
-
-// test("AudioInfo blob", async () => {
-//   const mock = vi.spyOn(console, "log").mockImplementation(() => void 0);
-//   const blobInfo = await AudioInfo.getInfo(new Blob());
-//   expect(mock).toHaveBeenCalledTimes(2);
-
-//   expect(blobInfo).toHaveProperty("title");
-//   expect(blobInfo).toHaveProperty("artists");
-//   expect(blobInfo).toHaveProperty("album");
-//   expect(blobInfo).toHaveProperty("albumartist");
-//   expect(blobInfo).toHaveProperty("track");
-//   expect(blobInfo).toHaveProperty("disk");
-//   expect(blobInfo).toHaveProperty("date");
-//   expect(blobInfo).toHaveProperty("genre");
-//   expect(blobInfo).toHaveProperty("picture");
-//   expect(blobInfo).toHaveProperty("sort");
-//   expect(blobInfo).toEqual(AudioInfo.getEmptyInfo());
-// });
