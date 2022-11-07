3707ab033fe8cd1334d2d55bce3019009cd7f82a
mikiymk <mikiymk001@gmail.com>
2022-11-07 16:53:32 +0900

    #122 アプリ全体のスナップショットを作る

---
+++ src/components/AppRoot/__tests__/AppRoot.test.tsx
@@ -0,0 +1,29 @@
+import { render } from "solid-js/web";
+import { expect, test, vi } from "vitest";
+
+import { MusicPlayer } from "../index";
+
+test("1 second", () => {
+  window.AudioContext = vi.fn();
+  window.fetch = vi.fn(() =>
+    Promise.resolve({
+      async json() {
+        /* eslint-disable camelcase */
+        return {
+          access_token: "access token",
+          expires_in: 3599,
+          refresh_token: "refresh token",
+          scope: "scope",
+          token_type: "token type",
+          files: [],
+          nextPageToken: undefined,
+        };
+      },
+    } as Response)
+  );
+
+  const root = document.createElement("div");
+  render(MusicPlayer, root);
+
+  expect(root).toMatchSnapshot();
+});
