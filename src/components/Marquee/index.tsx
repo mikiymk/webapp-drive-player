95eab1adcac7a311d66bca0f9c56ebf1a6d89ed4
mikiymk <mikiymk001@gmail.com>
2022-11-14 18:46:46 +0900

    #126 marqueeのリファクタリング

--- src/components/Marquee/index.tsx
+++ src/components/Marquee/index.tsx
@@ -1,19 +1,16 @@
-import { styleInner, styleMarquee } from "./style.css";
+import { styleInner, marquee } from "./style.css";
 
 import type { JSX } from "solid-js";
 
-export interface MarqueeProps {
+interface MarqueeProps {
   children: JSX.Element;
   class?: string;
 }
 
 /** CSSで横に動く */
 export const Marquee = (props: MarqueeProps) => {
-  const classes = () =>
-    props.class ? `${styleMarquee} ${props.class}` : styleMarquee;
-
   return (
-    <span class={classes()}>
+    <span class={props.class ? `${marquee} ${props.class}` : marquee}>
       <span class={styleInner}>{props.children}</span>
     </span>
   );
