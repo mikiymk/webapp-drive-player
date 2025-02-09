1d0cbae74eda17cb384eacd04f673f249eaca702
mikiymk <mikiymk001@gmail.com>
2025-02-09 15:42:05 +0900

    migrate eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,76 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import importEslint from "eslint-plugin-import";
+import solidEslint from "eslint-plugin-solid";
+
+export default tseslint.config([
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    ignores: ["**/*.vanilla.js"],
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tseslint.parser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        project: "tsconfig.json",
+      },
+    },
+
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"],
+      },
+
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+  },
+
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  importEslint.flatConfigs.recommended,
+  importEslint.flatConfigs.typescript,
+  solidEslint.configs["flat/typescript"],
+
+  {
+    rules: {
+      camelcase: "error",
+      eqeqeq: "error",
+      "max-statements": ["error", 30],
+      "no-constant-condition": ["error", { checkLoops: false }],
+      "no-empty-function": "warn",
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "no-throw-literal": "error",
+
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-namespace": "error",
+
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-empty-function": "off",
+    },
+  },
+  {
+    files: ["**/*.test.ts"],
+
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+]);
